0052ec52c9d39eb8fdee3ae900e89f757addbf34
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-06-17 20:58:25 +0200

    Added Google Style Python Docstrings
    Included Docstrings for documenting purposes following Google's
    Style Guide regarding Python Docstrings
    
    This Docstrings allowed to generate automatic documentation using
    Sphinx

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 import time
 from api_interactions import get_env_variable, ApiClient
 from astral_objects import Polyanet, Soloon, Cometh
+from utils import print_megaverse, print_goal_map
 
 def create_megaverse(api_client, goal_map):
+	"""Creates the expected megaverse based on the specified goal map
+
+	Args:
+		api_client(ApiClient): The client to interact with the API
+		goal_map (dict): The goal map that contains the expected megaverse
+
+	Returns:
+		list: A list of created megaverse objects
+	"""
 	megaverse_objects = []
 	for row_index, row in enumerate(goal_map['goal']):
 		for col_index, value in enumerate(row):
@@ -21,19 +31,18 @@ def create_megaverse(api_client, goal_map):
 
 	return megaverse_objects
 
-def print_megaverse(objects):
-	for object in objects:
-		print("MEGAVERSE:")
-		print(f"{object.__class__.__name__}: row {object.row} | column {object.column}")
-
 def main():
+	"""Main function to run the application
+
+	Retrieves environment variables, initializes the API Client,
+	retrieves the goal map, and creates the megaverse.
+	"""
 	candidate_id = get_env_variable('CANDIDATE_ID')
 	base_url = get_env_variable('API_URL')
 	api_client = ApiClient(base_url, candidate_id)
 
 	goal_map = api_client.get_goal_map()
 	if goal_map:
-		print("Goal Map:", goal_map)
 		objects = create_megaverse(api_client, goal_map)
 
 if __name__ == "__main__":
