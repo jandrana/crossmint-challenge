99c6e66d9bc95210b55d7d07b5d2592c14c946fc
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-06-18 00:02:16 +0200

    Changed html theme for sphinx documentation
        - New theme: sphinx_book_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,6 @@ release = '1.0'
 extensions = [
 	'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx_wagtail_theme'
 ]
 
 templates_path = ['_templates']
@@ -36,5 +35,5 @@ autodoc_default_options = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_wagtail_theme'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
