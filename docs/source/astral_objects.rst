d3e3107c094286d3d0a3499f2012165c31530fc5
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-06-17 21:14:55 +0200

    Added docs/ with Sphinx generated Documentation
    Includes:
     - /build/html folder with documentation
     - /source folder with configuration files for generating docs
     - make.bat and Makefile for building documentation
    
    Documentation generated from Docstrings present in src files

---
+++ docs/source/astral_objects.rst
@@ -0,0 +1,7 @@
+astral\_objects module
+======================
+
+.. automodule:: astral_objects
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
+project = 'crossmint-challenge'
+copyright = '2024, Ana Alejandra Castillejo Muñoz'
+author = 'Ana Alejandra Castillejo Muñoz'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+	'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "private-members": True
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
